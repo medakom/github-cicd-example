2527872a7204f856708cdc42981e6c2680efc121
medakom <elmokademr990@gmail.com>
2023-09-27 12:22:04 +0200

    code

---
+++ src/main/java/com/adichar/githubcicd/GithubCicdActionsApplication.java
@@ -0,0 +1,27 @@
+package com.adichar.githubcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to site web";
+	}
+//	echo "# github-cicd-example" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/medakom/github-cicd-example.git
+//	git push -u origin main
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adichar/githubcicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adichar.githubcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
